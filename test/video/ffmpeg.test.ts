c4cadcbe65ddd7546260c2d1d06fe12f993b5f12
James Hu <james.hu.ustc@hotmail.com>
2024-01-26 13:39:38 +1100

    chore: test scenario with optional arguments

--- test/video/ffmpeg.test.ts
+++ test/video/ffmpeg.test.ts
@@ -7,11 +7,11 @@ import { extractVideoFramesWithFfmpeg } from '../../src/video';
 
 describe('ffmpeg extractVideoFrames', () => {
   it('should work with a sample file', async () => {
-    rmSync('/tmp/000000.jpg');
-    rmSync('/tmp/000001.jpg');
-    rmSync('/tmp/000002.jpg');
-    rmSync('/tmp/000003.jpg');
-    rmSync('/tmp/000004.jpg');
+    rmSync('/tmp/000000.jpg', { force: true });
+    rmSync('/tmp/000001.jpg', { force: true });
+    rmSync('/tmp/000002.jpg', { force: true });
+    rmSync('/tmp/000003.jpg', { force: true });
+    rmSync('/tmp/000004.jpg', { force: true });
     expect(existsSync('/tmp/000000.jpg')).toBe(false);
     expect(existsSync('/tmp/000001.jpg')).toBe(false);
     expect(existsSync('/tmp/000002.jpg')).toBe(false);
@@ -33,5 +33,22 @@ describe('ffmpeg extractVideoFrames', () => {
     expect(existsSync('/tmp/000002.jpg')).toBe(true);
     expect(existsSync('/tmp/000003.jpg')).toBe(true);
     expect(existsSync('/tmp/000004.jpg')).toBe(true);
+  });
+
+  it('should work with optional arguments on a sample file', async () => {
+    rmSync('/tmp/000001.png', { force: true });
+    rmSync('/tmp/000003.png', { force: true });
+    expect(existsSync('/tmp/000001.png')).toBe(false);
+    expect(existsSync('/tmp/000003.png')).toBe(false);
+
+    const file = `${__dirname}/000001.mp4`;
+    const files = await extractVideoFramesWithFfmpeg(file, '/tmp', 2, 'png', 100, undefined, 1, 3);
+
+    expect(files.length).toBe(2);
+    expect(files[0]).toBe('000001.png');
+    expect(files[1]).toBe('000003.png');
+
+    expect(existsSync('/tmp/000001.png')).toBe(true);
+    expect(existsSync('/tmp/000003.png')).toBe(true);
   });
 });
