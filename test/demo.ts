88ff28ee9fc3b74123ac1f4cc2dd03f663ac2479
James Hu <james.hu.ustc@hotmail.com>
2024-02-03 22:57:18 +1100

    convert demo to plain ts

---
+++ test/demo.ts
@@ -0,0 +1,33 @@
+/* eslint-disable node/no-unpublished-import */
+import chalk from 'chalk';
+import readline from 'node:readline';
+
+import { ChatAboutVideo } from '../src';
+
+const rl = readline.createInterface({ input: process.stdin, output: process.stdout });
+const prompt = (question: string) => new Promise<string>((resolve) => rl.question(question, resolve));
+
+async function demo() {
+  const chat = new ChatAboutVideo({
+    openAiEndpoint: process.env.AZURE_OPENAI_API_ENDPOINT!,
+    openAiApiKey: process.env.AZURE_OPENAI_API_KEY!,
+    azureStorageConnectionString: process.env.AZURE_STORAGE_CONNECTION_STRING!,
+    openAiDeploymentName: 'gpt4vision',
+    storageContainerName: 'vision-experiment-input',
+    storagePathPrefix: 'video-frames/',
+  });
+
+  const conversation = await chat.startConversation(process.env.DEMO_VIDEO!);
+  
+  while(true) {
+    const question = await prompt('\nUser: ');
+    if (!question) {
+      continue;
+    }
+    const answer = await conversation.say(question);
+    console.log('\nAI:', answer);
+  }
+}
+
+// eslint-disable-next-line unicorn/prefer-top-level-await
+demo().catch((error) => console.log(chalk.red(JSON.stringify(error, null, 2))));
