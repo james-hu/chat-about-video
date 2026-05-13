b4625b26cafa612f986701671c6c98f72270ad84
James Hu <james.hu.ustc@hotmail.com>
2026-05-13 13:09:42 +1000

    fix: ffmpeg 7 exits with a non-zero code when the extraction point is out of range

--- src/video/ffmpeg.ts
+++ src/video/ffmpeg.ts
@@ -1,6 +1,6 @@
 /* eslint-disable max-params */
 import { inParallel } from '@handy-common-utils/promise-utils';
-import { execFile } from 'node:child_process';
+import { ExecFileException, execFile } from 'node:child_process';
 import fs from 'node:fs/promises';
 import path from 'node:path';
 import { promisify } from 'node:util';
@@ -43,9 +43,24 @@ export const extractVideoFramesWithFfmpeg: VideoFramesExtractor = async (
   await fs.mkdir(outputDir, { recursive: true });
   for (let i = startSec; (endSec == null || i < endSec) && (limit == null || relativePaths.length <= limit); i += intervalSec) {
     const fileName = `${i.toFixed(3).padStart(10, '0')}.${format}`;
-    const { stderr } = await execFileAsync(ffmpegPath, [...args1, `${i}`, ...args2, path.join(outputDir, fileName)]);
-    if (stderr && (stderr.includes('Output file is empty, nothing was encoded') || stderr.includes('Nothing was written into output file'))) {
+    const filePath = path.join(outputDir, fileName);
+    const result = await execFileAsync(ffmpegPath, [...args1, `${i}`, ...args2, filePath]).catch((error: ExecFileException) => error);
+    const { stderr } = result;
+    if (
+      typeof stderr === 'string' &&
+      (stderr.includes('Output file is empty, nothing was encoded') || stderr.includes('Nothing was written into output file'))
+    ) {
+      // The extraction point is out of range, so we just stop
       break;
+    }
+    if (
+      !(await fs.stat(filePath).then(
+        () => true,
+        () => false,
+      ))
+    ) {
+      // file not generated, there must be something wrong, so we panic
+      throw new Error(`FFmpeg failed to extract the frame at ${i} seconds: ${fileName}`, { cause: result });
     }
     relativePaths.push(fileName);
   }
